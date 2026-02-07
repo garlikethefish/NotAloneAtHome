b6345cd7dd6791bc9d43a7d866d52e6fcc47cc46
garlikethefish <106908475+garlikethefish@users.noreply.github.com>
2026-02-07 19:46:32 +0200

    tweaked shaders and added normal maps to objects

--- scripts/trash.gd
+++ scripts/trash.gd
@@ -3,7 +3,7 @@ class_name TrashObject
 
 @onready var spawnable: ISpawnable = $ISpawnable
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var trashRes = preload("res://sprites/trash.png")
+@onready var trashRes = preload("res://canvas_textures/trash1_texture.tres")
 @export var tapsTillDone := 5
 
 func _ready():
